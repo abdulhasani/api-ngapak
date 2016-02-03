3a978334a686e4cec5f7e2285b1e16abd5d0193f
iqbaloyonz <iqbal.oyonz@gmail.com>
2016-02-03 10:39:26 +0700

    Menambahkan class Person pada paket master data, model

--- app/models/master_data/Person.java
+++ app/models/master_data/Person.java
@@ -1,7 +1,56 @@
 package models.master_data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by User on 2/2/2016.
  */
+@Entity
+@Table(name = "person")
 public class Person {
+
+    @Id
+    @Column(name = "id_person", nullable = false, length = 15)
+    private String id;
+    @Column(name = "nama", nullable = false, length = 255)
+    private String nama;
+    @Column(name = "umur", nullable = false, length = 255)
+    private String umur;
+    @Column(name = "alamat", nullable = false, length = 255)
+    private String alamat;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getUmur() {
+        return umur;
+    }
+
+    public void setUmur(String umur) {
+        this.umur = umur;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
 }
