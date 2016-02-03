ad8aecae1d69bbc0190b5ea2e0ab536f08296bfc
iqbaloyonz <iqbal.oyonz@gmail.com>
2016-02-03 11:11:27 +0700

    database with table

--- database/ngapak-jti.sql
+++ database/ngapak-jti.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.3.9
 -- Dumped by pg_dump version 9.3.9
--- Started on 2016-02-03 11:05:11
+-- Started on 2016-02-03 11:09:10
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -14,7 +14,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
--- TOC entry 170 (class 3079 OID 11750)
+-- TOC entry 171 (class 3079 OID 11750)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -22,16 +22,41 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 1931 (class 0 OID 0)
--- Dependencies: 170
+-- TOC entry 1936 (class 0 OID 0)
+-- Dependencies: 171
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
 --
--- TOC entry 1930 (class 0 OID 0)
+-- TOC entry 170 (class 1259 OID 75791)
+-- Name: person; Type: TABLE; Schema: public; Owner: ngapak; Tablespace: 
+--
+
+CREATE TABLE person (
+);
+
+
+ALTER TABLE public.person OWNER TO ngapak;
+
+--
+-- TOC entry 1928 (class 0 OID 75791)
+-- Dependencies: 170
+-- Data for Name: person; Type: TABLE DATA; Schema: public; Owner: ngapak
+--
+
+
+
+--
+-- TOC entry 1935 (class 0 OID 0)
 -- Dependencies: 5
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
@@ -42,7 +67,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2016-02-03 11:05:11
+-- Completed on 2016-02-03 11:09:10
 
 --
 -- PostgreSQL database dump complete
