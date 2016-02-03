b83d100f46d41bb50dd0fe57f4311635f603fbf4
iqbaloyonz <iqbal.oyonz@gmail.com>
2016-02-03 13:06:33 +0700

    save person

--- app/service/CommandServiceImpl/PersonConnmandServiceImpl.java
+++ app/service/CommandServiceImpl/PersonConnmandServiceImpl.java
@@ -1,18 +1,28 @@
 package service.CommandServiceImpl;
 
 
+import converterEntity.ConvertPerson;
+import dao.personDao.PersonDao;
 import models.master_data.Person;
 import models.master_data.wrapper.PersonWrap;
 import service.CommandService.PersonCommandService;
+
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by User TEGAR on 2/3/2016.
  */
 public class PersonConnmandServiceImpl implements PersonCommandService {
 
+   private final PersonDao personDao = new PersonDao();
+
     @Override
     public Person submit(PersonWrap wrap) {
-
-        return null;
+        Person save = (Person) personDao.save(ConvertPerson.convertPerson(wrap));
+        return save;
     }
+
+
+
 }
